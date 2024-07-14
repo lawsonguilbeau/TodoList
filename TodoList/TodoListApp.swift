c328ca4ea1d88ad88cde413037e7e5590ec56602
lawsonguilbeau <146668871+JiggyMac12@users.noreply.github.com>
2024-07-14 17:50:49 -0400

    Initial commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Lawson Guilbeau on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
